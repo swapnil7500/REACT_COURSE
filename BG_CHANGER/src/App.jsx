97010a6e3f05926c76a867dadb0d57c6c0903109
swapnil_1800 <swapnilsureshgupta365@gmail.com>
2025-09-16 19:07:14 +0530

    adding color logic by hooks

--- BG_CHANGER/src/App.jsx
+++ BG_CHANGER/src/App.jsx
@@ -1,16 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("pink")
 
-  return (
-    <>
-   <h1>welcome to BG_CHANGER</h1>
-    </>
-  )
+const Blue = function() {
+  setColor("blue");
+}
+const Green = function() {
+  setColor("green");
+}
+const Black  = function() {
+  setColor("black");
+}
+ return (
+  <>
+  <h1 className='bg-amber-950 text-amber-100 m-1 p-1 border-amber-600 rounded-3xl'>WELCOME TO MY BG_CHANGER</h1>
+ <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+ <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+ <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2
+ rounded-3xl">
+  <button onClick={Blue} className='text-amber-100 ' style={{backgroundColor:"blue"}}>Blue</button>
+  <button on onClick={Green} className='text-amber-100 ' style={{backgroundColor:"green"}}>Green</button>
+  <button onClick={Black} className='text-amber-100 ' style={{backgroundColor:"black"}}>Black</button>
+  {/* we can also make a btn and add hooks like that */}
+  <button onClick={() => setColor("yellow")} className='brown' style={{backgroundColor:"yellow"}}>Yellow</button>
+
+ </div>
+ </div>
+ </div>
+
+ </>
+ )
+
 }
 
 export default App
